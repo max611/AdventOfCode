20a4929990822f74409fd31c010b7c8294a53abb
Max St-Onge <mstonge@lanets.ca>
2015-12-04 09:49:32 -0500

    prefered version of day 3

--- day3-part1.rb
+++ day3-part1.rb
@@ -1,43 +1,15 @@
-path = ""
+input = File.read('path.txt').split('')
 
-File.open('path.txt','r') do |f|
-   f.each_char do |c| # iterate on each character
-      path << c
-   end
-end
+x = 0; 
+y = 0; 
+a = {[0,0] => 1}; 
 
-x = y = 0
+input.each { |i| case i; 
+when '>'; x += 1; 
+when '<'; x -= 1; 
+when '^'; y -= 1; 
+when 'v'; y += 1; 
+end; 
+a[[x,y]] = 1 };
 
-houseDone = {}
-
-houseDone[x] = Hash.new(0)
-houseDone[x][y] = 1
-
-
-
-path.each_char do |z|
-	case z
-	when "v"
-		y +=1
-    when "^"
-		y -=1
-	when ">"
-		x += 1
-    when "<"
-		x -= 1
-	end
-
-    houseDone[x] = Hash.new(0) unless houseDone[x].is_a?(Hash)
-    houseDone[x][y] += 1
-end 
-
-total = 0
-
-houseDone.keys.each do |xx|
-  houseDone[xx].keys.each do |yy|
-    total += 1 if houseDone[xx][yy] > 0
-  end
-end
-
-
-puts total
+puts a.length
